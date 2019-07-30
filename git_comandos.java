0997207125583617ed2cfa880c1deffb02f29e9d
Paulo <prmarches@live.com>
2019-07-30 10:50:24 -0300

    adiciona versao

--- git_comandos.java
+++ git_comandos.java
@@ -1,3 +1,5 @@
+﻿Versao teste
+
 git config
 //Um dos comandos git mais usados ​​é o git config que pode ser usado para definir valores de configuração específicos do usuário como e-mail, algoritmo preferido para diff, nome de usuário e formato de arquivo etc. Por exemplo, o seguinte comando pode ser usado para definir o email:
 git config --global user.email sam@google.com
